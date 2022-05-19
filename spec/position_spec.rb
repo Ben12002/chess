8acdefbbb57d22225a20e8e79ab9a37fe0abc674
Benedict Tan <bendamian2012@gmail.com>
2022-05-19 14:59:13 -0700

    before implementing position class

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -8,4 +8,16 @@ describe Position do
     expect(pos1).to eq pos2
   end
 
+  it 'returns true' do
+    pos1 = Position.new(1,2)
+    pos2 = Position.new(1,2)
+    arr1 = [pos1, pos2]
+
+    pos3 = Position.new(1,2)
+    pos4 = Position.new(1,2)
+    arr2 = [pos3, pos4]
+    
+    expect(arr1).to eq arr2
+  end
+
 end
